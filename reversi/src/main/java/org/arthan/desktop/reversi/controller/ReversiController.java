bcd500ddc3c0279f63b08b51a4384a8a74ef3012
Artur Shamsiev <arthan1011@gmail.com>
2015-10-15 17:16:09 +0300

    fxml begin

---
+++ reversi/src/main/java/org/arthan/desktop/reversi/controller/ReversiController.java
@@ -0,0 +1,12 @@
+package org.arthan.desktop.reversi.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.TilePane;
+
+/**
+ * Created by ashamsiev on 15.10.2015
+ */
+public class ReversiController {
+
+    @FXML TilePane titlePane;
+}
