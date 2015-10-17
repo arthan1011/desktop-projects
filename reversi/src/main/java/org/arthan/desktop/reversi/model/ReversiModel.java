638c7fb783f892553198926a02810dbf3b49926e
arthan1011 <arthan1011@gmail.com>
2015-10-17 13:33:58 +0300

    getting board info from server

--- reversi/src/main/java/org/arthan/desktop/reversi/model/ReversiModel.java
+++ reversi/src/main/java/org/arthan/desktop/reversi/model/ReversiModel.java
@@ -1,13 +1,18 @@
 package org.arthan.desktop.reversi.model;
 
-import javafx.beans.binding.Binding;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.binding.NumberExpression;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableBooleanValue;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by ashamsiev on 07.10.2015
@@ -30,18 +35,38 @@ public class ReversiModel {
                 board[i][j] = new SimpleObjectProperty<>(OWNER.NONE);
             }
         }
-        initBoard();
+        refresh();
     }
 
-    private void initBoard() {
-        int center_1 = BOARD_SIZE / 2 - 1;
-        int center_2 = BOARD_SIZE / 2;
-
-        board[center_1][center_1].setValue(OWNER.WHITE);
-        board[center_1][center_2].setValue(OWNER.BLACK);
-        board[center_2][center_1].setValue(OWNER.BLACK);
-        board[center_2][center_2].setValue(OWNER.WHITE);
+    private void refresh() {
+        populate(retrieveBoardInfo());
     }
+
+    private byte[] retrieveBoardInfo() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080/wild/rest/reversi");
+        Response response = target.request().get();
+        InputStream inputStream = response.readEntity(InputStream.class);
+
+        byte[] bytes = new byte[response.getLength()];
+        try {
+            inputStream.read(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+
+    public void populate(byte[] boardArray) {
+        for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+            int x = boardArray[i*3];
+            int y = boardArray[i*3 + 1];
+            byte ownerCode = boardArray[i*3 + 2];
+            OWNER owner = ownerCode == 0 ? OWNER.NONE : ownerCode == 1 ? OWNER.BLACK : OWNER.WHITE;
+            board[x][y].setValue(owner);
+        }
+    }
+
 
     public NumberExpression getScore(OWNER owner) {
         NumberExpression score = new SimpleIntegerProperty(0);
@@ -63,6 +88,7 @@ public class ReversiModel {
     }
 
     public BooleanBinding legalMove(int x, int y) {
+
         return board[x][y].isEqualTo(OWNER.NONE).and(
                     canFlip(x, y, 1, 1).or(
                     canFlip(x, y, 1, -1).or(
@@ -116,7 +142,6 @@ public class ReversiModel {
                 board[i][j].setValue(OWNER.NONE);
             }
         }
-        initBoard();
         turn.setValue(OWNER.BLACK);
     }
 
