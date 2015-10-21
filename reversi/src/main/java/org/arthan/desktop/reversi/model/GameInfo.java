d67c5d0daa151312161cdc0e1d6895a45ab08f36
Artur Shamsiev <arthan1011@gmail.com>
2015-10-21 14:28:26 +0300

    game info toString humanreadable

--- reversi/src/main/java/org/arthan/desktop/reversi/model/GameInfo.java
+++ reversi/src/main/java/org/arthan/desktop/reversi/model/GameInfo.java
@@ -40,8 +40,26 @@ public class GameInfo {
 
     @Override
     public String toString() {
-        return "GameInfo{" +
-                "gameInfo='" + Arrays.toString(gameInfo) + '\'' +
-                '}';
+        return "GameInfo{gameInfo=" + getBoardString() + '}';
+    }
+
+    private String getBoardString() {
+        StringBuilder result = new StringBuilder("\n");
+        for (int i = 1; i < gameInfo.length + 1; i++) {
+            byte cell = gameInfo[i-1];
+            if (i == 65) {
+                result.append(cell == 1 ? "BLACK" : "WHITE");
+                break;
+            }
+            if (cell == 0)
+                result.append("-");
+            if (cell == 1)
+                result.append("8");
+            if (cell == 2)
+                result.append("0");
+            if (i % 8 == 0)
+                result.append("\n");
+        }
+        return result.toString();
     }
 }
